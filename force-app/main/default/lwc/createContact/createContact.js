5892de2709aae7a57cb80f72c1cadbc3c26c83cb
Lucas Santana <lucas.bsi@outlook.com>
2021-10-04 00:38:02 -0300

    finalizado teste de tratamento cep (retira caracteres)

--- force-app/main/default/lwc/createContact/createContact.js
+++ force-app/main/default/lwc/createContact/createContact.js
@@ -8,6 +8,7 @@ export default class RecordFormExample extends LightningElement {
     message = ''
     variant = 'alert'
     mode = 'dismissible'
+    cep = '';
     
     handleSuccess(event) {
         this.showToast();
@@ -87,4 +88,89 @@ export default class RecordFormExample extends LightningElement {
         });
         this.dispatchEvent(evt);
     }
+
+    /*-----------ViaCEP Webservice---------*/
+    limpa_formulário_cep() {
+        //Limpa valores do formulário de cep.
+        document.getElementById('rua').value=("");
+        document.getElementById('bairro').value=("");
+        document.getElementById('cidade').value=("");
+        document.getElementById('uf').value=("");
+        document.getElementById('ibge').value=("");
+    }
+
+    meu_callback(conteudo) {
+        if (!("erro" in conteudo)) {
+            //Atualiza os campos com os valores.
+            document.getElementById('rua').value=(conteudo.logradouro);
+            document.getElementById('bairro').value=(conteudo.bairro);
+            document.getElementById('cidade').value=(conteudo.localidade);
+            document.getElementById('uf').value=(conteudo.uf);
+            document.getElementById('ibge').value=(conteudo.ibge);
+        } //end if.
+        else {
+            //CEP não Encontrado.
+            limpa_formulário_cep();
+            alert("CEP não encontrado.");
+        }
+    }
+    pesquisacep2(event){
+        this.cep = event.target.value;
+    }
+    
+    pesquisacep() {
+        //alert('abc'+document.getElementById("cep").value);
+        //Nova variável "cep" somente com dígitos.
+        //var cep = valor.replace(/\D/g, '');
+        //this.cep = event.target.value;
+        //this.showNotification();
+        //var cep = document.getElementById('cep').value;
+        //var cep = document.querySelector("#cep");
+        // var cep = document.getElementById("cep").value;
+        //this.cep = document.getElementById("cep").value;
+//alert(st);
+         alert(this.cep);
+         this.cep = this.cep.replace(/\D/g, '');
+         alert("cep - formatado"+this.cep);
+
+        //Verifica se campo cep possui valor informado.
+        if (this.cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(this.cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                document.getElementById('rua').value="...";
+                document.getElementById('bairro').value="...";
+                document.getElementById('cidade').value="...";
+                document.getElementById('uf').value="...";
+                document.getElementById('ibge').value="...";
+
+                //Cria um elemento javascript.
+                var script = document.createElement('script');
+
+                //Sincroniza com o callback.
+                script.src = 'https://viacep.com.br/ws/'+ this.cep + '/json/?callback=meu_callback';
+                alert(script.src);
+
+                //Insere script no documento e carrega o conteúdo.
+                document.body.appendChild(script);
+
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            alert("cep nulooo");
+            limpa_formulário_cep();
+        }
+    };
+    
 }
